9d57f40efb9b5a8c313d172e3e33b60821bfd01c
drumath <2237k.drumath@gmail.com>
2023-10-09 17:00:37 +0900

    :soarkles: github actions用の設定をplaygroundに追加

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -2,15 +2,18 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  optimizeDeps: {
-    include: ["lib-a"],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/lib-a/, /node_modules/],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/babyuew-monorepo-testbed/" : "",
+    plugins: [vue()],
+    optimizeDeps: {
+      include: ["lib-a"],
     },
-  },
+    build: {
+      commonjsOptions: {
+        include: [/lib-a/, /node_modules/],
+      },
+    },
+  };
 });
 
